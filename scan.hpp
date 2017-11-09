029e6c223320d4ccdc61a40dc05100442575f799
rincemust <aadi.naik@gmail.com>
2017-11-09 15:28:01 +0530

    Added ability to accept doubles with a mantissa of 0

--- scan.hpp
+++ scan.hpp
@@ -17,38 +17,50 @@ namespace Scanner {
 	//an adaptation of cin to make accepting input less hectic
 	class Scan {
 	private:
+		//function to clear the buffer stream
 		void fix() {
 			std::cin.clear();
 			std::cin.ignore(32767, '\n');
 		}
 
+		//function to set the value of a particular x
 		template<class T>
 		void set(T& x);
-
+		
 	public:
+		//function that will be used by the user
 		template<class T>
 		T next() {
-			T x;
-			set(x);
-			return x;
+			T data;
+			set(data);
+			return data;
 		}
 	};
-
+	
 	template<class T>
-	inline void Scan::set(T& x) {
+	void Scan::set(T& x) {
 		std::cin >> x;
 		if(std::cin.fail()) {
 			fix();
 			throw InvalidInputException();
 		}
 	}
-	
+
+	//for integers- check if there is a period
+	//if so, and if the decimal numbers are not 0, fix
+	//else return
 	template<>
-	inline void Scan::set(int& x) {
+	void Scan::set(int& x) {
 		std::cin >> x;
-		if(std::cin.fail() || std::cin.peek() == '.') {
+		if(std::cin.fail()) {
 			fix();
 			throw InvalidInputException();
+		} else if(std::cin.peek() == '.') {
+			double dec = next<double>();
+			if(dec != 0) {
+				fix();
+				throw InvalidInputException();
+			}
 		}
 	}
 }
