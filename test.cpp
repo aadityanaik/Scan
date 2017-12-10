b21acb480df77fa50f18fb02b3c81bf6bcc5526b
rincemust <aadi.naik@gmail.com>
2017-12-10 13:21:27 +0530

    Updated test.cpp

--- test.cpp
+++ test.cpp
@@ -4,11 +4,28 @@
 
 int main() {
 	int x;
-	Scanner::Scan input;
 
+	//Using Scanner::next<type>
 	std::cout << "Enter an integer\n";
 
-	x = Scanner::next<int>("Integer expected");
-
+	x = scanner::next<int>("Integer expected");
 	std::cout << "Congratulations! You entered an integer- " << x << std::endl;
+
+	//Using Scanner::Scan
+	double y;
+	std::cout << "Enter a double this time\n";
+
+	scanner::Scan input;
+	while(true) {
+		try {
+			y = input.next<double>();
+			break;
+		} catch(const scanner::InvalidInputException& e) {
+			std::cerr << "Double expected\n";
+		}
+	}
+
+	std::cout << "Congratulations! You entered a double- " << y << "\n";
+
+	return 0;
 }
